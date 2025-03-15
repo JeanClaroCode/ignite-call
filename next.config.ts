e1075f50d108c3327a00837d630bc7193c889552
JeanClaroCode <jeanclarodev@gmail.com>
2025-03-15 20:37:26 -0300

    add: modularize phophor-icons import

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  modularizeImports: {
+    '@phosphor-icons/react': {
+      transform: '@phosphor-icons/react/{{member}}',
+    },
+  },
 }
 
 export default nextConfig
